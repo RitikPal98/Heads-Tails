94e92a7b14617b417adeb4d9f7bb98cb992b711c
RitikPal98 <62811642+RitikPal98@users.noreply.github.com>
2020-05-30 23:23:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-var randomnumber = Math.floor(Math.random()*100) + 1;
-if(randomnumber%2 === 0){
-  document.querySelector("h1").innerHTML = "Head Wins";
-  document.querySelector("img").setAttribute("src", "images/head.png");
-  document.querySelector("p").innerHTML ="Refresh to Play Again!";
+document.querySelector("button").addEventListener("click", imagechanger);
+
+document.querySelector("button").addEventListener("click", alterno);
+
+function alterno(){var randomnumber = Math.floor(Math.random()*100) + 1;
 
 }
-else{
-  document.querySelector("h1").innerHTML = "Tail Wins";
-  document.querySelector("img").setAttribute("src", "images/tail.png");
-  document.querySelector("p").innerHTML ="Now Refresh to Play Again! ";
 
+function imagechanger(){
+  var randomnumber = Math.floor(Math.random()*100) + 1;
+  if (randomnumber%2 === 0){
+document.querySelector("img").setAttribute("src", "images/head.png");
+document.querySelector("h1").innerHTML = "Head Wins";
+document.querySelector("p").innerHTML ="Press Start to Playagain";
+
+
+
+  }else {
+    document.querySelector("img").setAttribute("src", "images/tail.png");
+    document.querySelector("h1").innerHTML = "Tail Wins";
+    document.querySelector("p").innerHTML ="Press Start to Playagain";
+
+
+
+  }
 }
